93a74bd963c482e9da1b62c9c79b7e2aa809651c
Timothy Caro-Bruce <tim@wawd.com>
2019-01-15 01:29:23 +0000

    fix AK people scraper for 2019

--- openstates/ak/__init__.py
+++ openstates/ak/__init__.py
@@ -23,38 +23,45 @@ class Alaska(Jurisdiction):
     }
     legislative_sessions = [
         {
-            "_scraped_name": "The 27th Legislature (2011-2012)",
+            "_scraped_name": "27th Legislature (2011-2012)",
             "identifier": "27",
             "name": "27th Legislature (2011-2012)"
         },
         {
-            "_scraped_name": "The 28th Legislature (2013-2014)",
+            "_scraped_name": "28th Legislature (2013-2014)",
             "identifier": "28",
             "name": "28th Legislature (2013-2014)"
         },
         {
-            "_scraped_name": "The 29th Legislature (2015-2016)",
+            "_scraped_name": "29th Legislature (2015-2016)",
             "identifier": "29",
             "name": "29th Legislature (2015-2016)"
         },
         {
-            "_scraped_name": "The 30th Legislature (2017-2018)",
+            "_scraped_name": "30th Legislature (2017-2018)",
             "end_date": "2017-04-16",
             "identifier": "30",
             "name": "30th Legislature (2017-2018)",
             "start_date": "2017-01-17"
+        },
+        {
+            "_scraped_name": "31st Legislature (2019-2020)",
+            "end_date": "2020-12-31",
+            "identifier": "31",
+            "name": "31st Legislature (2019-2020)",
+            "start_date": "2019-01-15"
         }
     ]
     ignored_scraped_sessions = [
-        "The 26th Legislature (2009-2010)",
-        "The 25th Legislature (2007-2008)",
-        "The 24th Legislature (2005-2006)",
-        "The 23rd Legislature (2003-2004)",
-        "The 22nd Legislature (2001-2002)",
-        "The 21st Legislature (1999-2000)",
-        "The 20th Legislature (1997-1998)",
-        "The 19th Legislature (1995-1996)",
-        "The 18th Legislature (1993-1994)"
+        "26th Legislature (2009-2010)",
+        "25th Legislature (2007-2008)",
+        "24th Legislature (2005-2006)",
+        "23rd Legislature (2003-2004)",
+        "22nd Legislature (2001-2002)",
+        "21st Legislature (1999-2000)",
+        "20th Legislature (1997-1998)",
+        "19th Legislature (1995-1996)",
+        "18th Legislature (1993-1994)"
     ]
 
     def get_organizations(self):
@@ -70,6 +77,6 @@ class Alaska(Jurisdiction):
 
     def get_session_list(self):
         return url_xpath(
-            'http://www.legis.state.ak.us/basis/start.asp',
-            "//div[@id='rightnav']//a[contains(@href, 'start.asp?session=')]//nobr/text()"
+            'http://www.akleg.gov/basis/Home/Archive',
+            "//div[@id='fullpage']//a[contains(@href, 'BillsandLaws')]/text()"
         )
