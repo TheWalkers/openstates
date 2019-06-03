7de6f3332b4f4ceca437c8a195ee07be0b727e42
Timothy Caro-Bruce <tim@wawd.com>
2019-06-03 23:43:35 +0000

    MN: remove no-longer needed special case

--- openstates/mn/people.py
+++ openstates/mn/people.py
@@ -111,11 +111,6 @@ class RepList(Page):
         name_match = re.match(r'^(.+)\(([0-9]{2}[AB]), ([A-Z]+)\)$', name_text)
         name = name_match.group(1).strip()
 
-        if date.today() > date(2019, 6, 1):
-            raise Exception("Check to see if this special case can be removed!")
-        if name == 'Jason Rarick':  # has moved on to senate, site not updated
-            return
-
         district = name_match.group(2).lstrip('0').upper()
         party_text = name_match.group(3)
         party = PARTIES[party_text]
