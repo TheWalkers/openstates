35ea7347601f1bb0bae1af6126b43a6040c47ca1
Timothy Caro-Bruce <tim@wawd.com>
2020-01-10 21:54:13 +0000

    MD: new website, update scraper accordingly

--- openstates/md/people.py
+++ openstates/md/people.py
@@ -6,66 +6,82 @@ from pupa.scrape import Person, Scraper
 
 def _get_table_item(doc, name):
     """ fetch items out of table that has a left column of th """
-    return doc.xpath('//th[contains(text(), "%s")]/following-sibling::td' % name)[0]
+    return doc.xpath('//dt[contains(text(), "%s")]/following-sibling::dd' % name)[0]
 
 
 class MDPersonScraper(Scraper):
-    def scrape(self, chamber=None):
-        url = (
-            "http://mgaleg.maryland.gov/webmga/frmmain.aspx?pid=legisrpage&tab=subject6"
-        )
+    urls = {
+        "lower": "http://mgaleg.maryland.gov/mgawebsite/Members/Index/house",
+        "upper": "http://mgaleg.maryland.gov/mgawebsite/Members/Index/senate",
+    }
 
+    def scrape(self, chamber=None):
+        chambers = [chamber] if chamber is not None else ["upper", "lower"]
+        if "upper" in chambers:
+            yield from self.scrape_table("upper")
+        if "lower" in chambers:
+            yield from self.scrape_table("lower")
+
+    def scrape_table(self, chamber):
+        url = self.urls[chamber]
         html = self.get(url).text
         doc = lxml.html.fromstring(html)
         doc.make_links_absolute(url)
-        sen_tbl, house_tbl = doc.xpath(
-            '//div[@class="legislrlist"]//table[@class="grid"]'
-        )
 
-        chambers = [chamber] if chamber is not None else ["upper", "lower"]
-        if "upper" in chambers:
-            yield from self.scrape_table("upper", sen_tbl)
-        if "lower" in chambers:
-            yield from self.scrape_table("lower", house_tbl)
+        seen = set()
 
-    def scrape_table(self, chamber, tbl):
-        # skip first
-        for row in tbl.xpath("tr")[1:]:
-            leg_a, district, _, _ = row.xpath("td")
-            district = district.text
-            name = leg_a.text_content().strip()
-            if name.lower() == "to be announced":
+        for row in doc.xpath('//div[contains(@class, "member-index-cell")]/div/div'):
+
+            img_cell, text_cell = row.getchildren()
+
+            if "to be announced" in text_cell.text_content().lower():
                 continue
-            leg_url = leg_a.xpath("a/@href")[0]
+
+            leg_a = text_cell.xpath('.//a')[0]
+            leg_url = leg_a.attrib['href']
+            name = leg_a.text
+
+            district = re.search(
+                r"District (\d{1,2}[ABCD]?)",
+                text_cell.text_content()
+            ).group(1)
+
+            key = name + district
+            if key in seen:  # leadership listed twice, skip the 2nd
+                continue
+            seen.add(key)
+
+            photo_url = img_cell.xpath("a/img/@src")[0]
 
             # get details
             html = self.get(leg_url).text
             ldoc = lxml.html.fromstring(html)
             ldoc.make_links_absolute(leg_url)
 
-            party = _get_table_item(ldoc, "Party Affiliation:").text
+            party = _get_table_item(ldoc, "Party").text
             if party == "Democrat":
                 party = "Democratic"
-            addr_lines = _get_table_item(ldoc, "Annapolis Address:").xpath("text()")
-            address = []
-            phone = None
-            fax = None
-            for line in addr_lines:
-                if "Phone:" in line:
-                    phone = re.findall(r"Phone: (\d{3}-\d{3}-\d{4})", line)[0]
-                elif "Fax:" in line:
-                    # Number oddities: one has two dashes, one has a dash and then a space.
-                    line = line.replace("--", "-").replace("- ", "-")
-                    fax = re.findall(r"Fax: (\d{3}-\d{3}-\d{4})", line)[0]
-                else:
-                    address.append(line)
+            capitol_info = _get_table_item(ldoc, "Annapolis Info")
+            addr_lines, phone_lines = capitol_info.xpath("dl/dd")
+
+            address = [s.strip() for s in addr_lines.text_content().split('\n') if s.strip()]
             address = "\n".join(address)
 
-            email = ldoc.xpath('//a[contains(@href, "mailto:")]/text()')
+            phone = None
+            fax = None
+            for line in phone_lines.text_content().split('\n'):
+                if "Phone" in line:
+                    phone = re.findall(r"Phone (\d{3}-\d{3}-\d{4})", line)[0]
+                elif "Fax" in line:
+                    # Number oddities: one has two dashes, one has a dash and then a space.
+                    line = line.replace("--", "-").replace("- ", "-")
+                    fax = re.findall(r"Fax (\d{3}-\d{3}-\d{4})", line)[0]
+
+            email = ldoc.xpath('//a[contains(@href, "mailto:")]/@href')
             if not email:
                 email = None
             elif len(email) == 1:
-                email = email[0].strip()
+                email = re.match(r"mailto:([^?]+)", email[0]).group(1)
             else:
                 raise AssertionError("Multiple email links found on page")
 
